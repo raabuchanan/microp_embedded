3abd9f4fd0acea1ed15dbe7404179929a3423806
Russell Buchanan <russell.buchanan@mail.mcgill.ca>
2016-03-24 15:35:45 -0400

    Fix gitignore to exclude compiled folders

--- Discovery/Objects/kalmanfilter_c.d
+++
@@ -1,4 +0,0 @@
-.\objects\kalmanfilter_c.o: Sources\kalmanfilter_c.c
-.\objects\kalmanfilter_c.o: .\Headers\kalmanfilter_c.h
-.\objects\kalmanfilter_c.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\kalmanfilter_c.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
--- Discovery/Objects/util.d
+++
@@ -1,3 +0,0 @@
-.\objects\util.o: Sources\util.c
-.\objects\util.o: .\Headers\util.h
-.\objects\util.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
